33f83ed2d6fada63aeb7d7f6d7029e1c7147f1d0
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 21:06:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def make_divisors_array(integer)
   divisors = Array.new
     integer_sqrt = Math.sqrt(integer)
     for counter in 1..integer_sqrt do
-      
+      divisors[counter-1] = counter
 end 
 
 
