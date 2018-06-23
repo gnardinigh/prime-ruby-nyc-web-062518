b89fc6f20ae6f913a3a05281bff8d8e67b4a1de9
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 21:05:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
+
+
+
 def prime?(integer)
   if integer<=1
     return false 
   else
-    divisors = Array.new
-    integer_sqrt = Math.sqrt(integer)
-    for counter in 1..integer_sqrt do
+    
       
     
     
